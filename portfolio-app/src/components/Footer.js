616789afbfe799856f987ff9eb9a92df5acdabb8
Hoefer <ningboninja@gmail.com>
2022-08-23 19:51:02 -0700

    initial set up

---
+++ portfolio-app/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <div>
+            <h1>Please Click on these links</h1>
+            <p>Other stuff</p>
+        </div>
+    )
+}
+
+
+
+export default Footer;
