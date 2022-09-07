c7354840fbd6949f1fac52e179d7c72e2d2b7e12
Hoefer <ningboninja@gmail.com>
2022-09-06 22:31:19 -0700

    testing heroku deploy

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import "../styles/footer.css"
 function Footer() {
     return (
         <div className='footer'>
-            <a href="https://github.com/hoeferm2"><img src="../../images/favicon/gitHub-icon.png" alt="gitHub" /></a>
+            <a href="https://github.com/hoeferm2"><img src="public\images\favicon\gitHub-icon.png" alt="gitHub" /></a>
             <a href="https://www.linkedin.com/in/matthew-hoefer-28160997/"><img src="../../images/favicon/LI-In-Bug.png" alt="linkedIn" /></a>
             <a href="mailto: ningboninja@gmail.com"><img src="../../images/favicon/gmail.png" alt="email" /></a>
             <a href="google.com"><img src="../../images/favicon/resume.png" alt='My Resume'/></a>
